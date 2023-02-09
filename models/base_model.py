40b0969fcca071d9925167e53664854a854d9794
Amadu <amaduhamzajaarah300@gmail.com>
2023-02-09 14:42:50 +0000

    model

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel:
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
-    def save(self) :
+    def save(self):
         created_at = datetime.datetime.now()
 
     def  to_dict(self):
@@ -21,4 +21,4 @@ class BaseModel:
         myDict["__class__"] = self.__class__.__name__
         myDict["created_at"] = self.created_at.isoformat()
         myDict["updated_at"] = self.updated_at.isoformat()
-        return dictionary
+        return myDict
