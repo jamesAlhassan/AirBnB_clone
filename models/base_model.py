999d0efe8c86c880c9654c914c6c6436171648db
James Alhassan <jamesalhassan052@gmail.com>
2023-02-09 15:22:42 +0000

    Update base_mode.py

--- models/base_model.py
+++ models/base_model.py
@@ -5,13 +5,8 @@ from datetime import datetime as dt
 
 
 class BaseModel:
-    '''
-    def __init__(self, id, created_at, updated_at):
-        self.id = str(uuid.uuid4())
-        created_at = datetime.now()
-        updated_at = datetime.now()'''
-    
-    def  __init__(self, *args, **kwargs):
+
+    def __init__(self, *args, **kwargs):
         if kwargs:
             for key, item in kwargs.items():
                 if key != "__class__":
@@ -24,19 +19,15 @@ class BaseModel:
             self.created_at = dt.now()
             self.updated_at = dt.now()
 
-
-    
     def __str__(self):
         return f'[{type(self).__name__}, {self.id}, {self.__dict__}]'
 
     def save(self):
         self.updated_at = dt.now()
 
-
      def to_dict(self):
-        my_dict = self.__dict__
+        my_dict = self.__dict__.copy()
         my_dict["__class__"] = type(self).__name__
         my_dict["created_at"] = my_dict["created_at"].isoformat()
         my_dict["updated_at"] = my_dict["updated_at"].isoformat()
         return my_dict
-
