42dd9a42051b38c96bd987a94c765f24ddb218c3
James Alhassan <jamesalhassan052@gmail.com>
2023-02-12 06:34:22 +0000

    Add tests/test_console.py

---
+++ tests/test_console.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module for TestHBNBCommand class."""
+
+from console import HBNBCommand
+from models.engine.file_storage import FileStorage
+import unittest
+import datetime
+from unittest.mock import patch
+import sys
+from io import StringIO
+import re
+import os
