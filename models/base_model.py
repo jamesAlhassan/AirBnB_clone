dc5c2681af7ce0ce02def7a441cc76b8876ae29f
James Alhassan <jamesalhassan052@gmail.com>
2023-02-12 09:36:21 +0000

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,37 +1,42 @@
 #!/usr/bin/python3
+'''base_model script'''
 
-import uuid
-from datetime import datetime
+from uuid import uuid4 as ui
+from datetime import datetime as dt
 from models import storage
 
 
 class BaseModel:
+    '''Base class which all other classes inherit from'''
     def __init__(self, *args, **kwargs):
         if kwargs is not None and kwargs != {}:
             for key in kwargs:
                 if key == "created_at":
-                    self.__dict__["created_at"] = datetime.strptime(
+                    self.__dict__["created_at"] = dt.strptime(
                         kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.strptime(
+                    self.__dict__["updated_at"] = dt.strptime(
                         kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     self.__dict__[key] = kwargs[key]
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+            self.id = str(ui())
+            self.created_at = dt.now()
+            self.updated_at = dt.now()
             storage.new(self)
 
     def __str__(self):
+        '''Returns a string representation of the class'''
         return "[{}] ({}) {}".\
             format(type(self).__name__, self.id, self.__dict__)
 
     def save(self):
-        self.updated_at = datetime.now()
+        '''Updates the public the public instance with the current date'''
+        self.updated_at = dt.now()
         storage.save()
 
     def to_dict(self):
+        '''Returns a dict with the keys, values of __dict__'''
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = type(self).__name__
         my_dict["created_at"] = my_dict["created_at"].isoformat()
