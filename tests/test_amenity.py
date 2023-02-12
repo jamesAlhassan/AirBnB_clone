9799b0e057db031dc5ee14bae6c8662712fc15db
James Alhassan <jamesalhassan052@gmail.com>
2023-02-12 14:33:23 +0000

    Update test_amenity.py

--- tests/test_amenity.py
+++ tests/test_amenity.py
@@ -11,3 +11,22 @@ from models.engine.file_storage import FileStorage
 import os
 from models import storage
 from models.base_model import BaseModel
+
+class TestAmenity(unittest.TestCase):
+
+    """Test Cases for the Amenity class."""
+
+    def setUp(self):
+        """Sets up test methods."""
+        pass
+
+    def tearDown(self):
+        """Tears down test methods."""
+        self.resetStorage()
+        pass
+
+    def resetStorage(self):
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
