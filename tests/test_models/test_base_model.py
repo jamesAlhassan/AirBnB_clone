44ed19d734b2d624013456e7476bb494adf16558
James Alhassan <jamesalhassan052@gmail.com>
2023-02-12 14:36:47 +0000

    Update test_base_model.py

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -11,3 +11,23 @@ import re
 import time
 import unittest
 import uuid
+
+
+class TestBaseModel(unittest.TestCase):
+
+    """Test Cases for the BaseModel class."""
+
+    def setUp(self):
+        """Sets up test methods."""
+        pass
+
+    def tearDown(self):
+        """Tears down test methods."""
+        self.resetStorage()
+        pass
+
+    def resetStorage(self):
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
