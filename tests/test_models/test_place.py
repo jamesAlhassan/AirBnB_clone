fd31395fcc5a3e0f1d79b6fb9abc01b1597a5756
James Alhassan <jamesalhassan052@gmail.com>
2023-02-12 14:44:47 +0000

    Add test_place

---
+++ tests/test_models/test_place.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Unittest module for the Place Class."""
+
+import unittest
+from datetime import datetime
+import time
+from models.place import Place
+import re
+import json
+from models.engine.file_storage import FileStorage
+import os
+from models import storage
+from models.base_model import BaseModel
+
+
+class TestPlace(unittest.TestCase):
+
+    """Test Cases for the Place class."""
+
+    def setUp(self):
+        """Sets up test methods."""
+        pass
+
+    def tearDown(self):
+        """Tears down test methods."""
+        self.resetStorage()
+        pass
+
+    def resetStorage(self):
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
