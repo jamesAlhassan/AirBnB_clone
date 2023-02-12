8f19cf55c1dbf66ed846cb6b558e8437cea48197
James Alhassan <jamesalhassan052@gmail.com>
2023-02-12 09:40:16 +0000

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+'''Package initialization'''
 
-from engine.file_storage import FileStorage
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
