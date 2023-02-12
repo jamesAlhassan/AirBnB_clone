64c4c1f5f7499ce6d2f791d9625fd5224b7912f5
James Alhassan <jamesalhassan052@gmail.com>
2023-02-12 14:36:21 +0000

    Add test_base_model.py

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Unittest module for the BaseModel Class."""
+
+from models import storage
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from datetime import datetime
+import json
+import os
+import re
+import time
+import unittest
+import uuid
