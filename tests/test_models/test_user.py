5f442fe075c4a9eb12c2ccffd51218c581e1bd82
James Alhassan <jamesalhassan052@gmail.com>
2023-02-12 14:54:10 +0000

    Add test_state.py

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Unittest module for the User Class."""
+
+import unittest
+from datetime import datetime
+import time
+from models.user import User
+import re
+import json
+from models.engine.file_storage import FileStorage
+import os
+from models import storage
+from models.base_model import BaseModel
+
