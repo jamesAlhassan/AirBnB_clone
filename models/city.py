a14ee3a344083f39c1f1c73ed6e1f025a667daab
James Alhassan <jamesalhassan052@gmail.com>
2023-02-12 20:56:28 +0000

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
+''' City class that inherits from BaseModel '''
 
 from models.base_model import BaseModel
 
-''' City class that inherits from BaseModel '''
 
 class City(BaseModel):
-
     ''' blue print for City object'''
-    
+
     state_id = ""
     name = ""
