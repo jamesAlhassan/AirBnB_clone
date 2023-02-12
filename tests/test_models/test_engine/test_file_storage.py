dfd13283dada790db7eb36e0db2977d2cbbbc06c
James Alhassan <jamesalhassan052@gmail.com>
2023-02-12 15:08:27 +0000

    Add test_file_storage.py

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,34 @@
+
+
+#!/usr/bin/python3
+"""Unittest module for the FileStorage class."""
+
+import unittest
+from datetime import datetime
+import time
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from models import storage
+import re
+import json
+import os
+
+
+class TestFileStorage(unittest.TestCase):
+    """Test Cases for the FileStorage class."""
+
+    def setUp(self):
+        """Sets up test methods."""
+        pass
+
+    def resetStorage(self):
+        """Resets FileStorage data."""
+        FileStorage._FileStorage__objects = {}
+        if os.path.isfile(FileStorage._FileStorage__file_path):
+            os.remove(FileStorage._FileStorage__file_path)
+
+    def tearDown(self):
+        """Tears down test methods."""
+        self.resetStorage()
+        pass
+
