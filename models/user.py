56bb96da4374c48e51bdf64c015d3394755a44fe
James Alhassan <jamesalhassan052@gmail.com>
2023-02-12 21:04:53 +0000

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
+''' User class that inherits from BaseModel '''
 
 from models.base_model import BaseModel
 
-''' User class that inherits from BaseModel ''' 
 
 class User(BaseModel):
-
     ''' Blue print for User object '''
-    
+
     email = ""
     password = ""
     first_name = ""
